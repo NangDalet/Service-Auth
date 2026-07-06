3660955ed29a293c9a5ed45ca158d98dd8e1af49
Nang Dalet <nangdalet@gmail.com>
2026-07-06 20:51:26 +0700

    Fix auth Swagger server URL

--- src/main/java/com/dt/student/register/config/SwaggerConfig.java
+++ src/main/java/com/dt/student/register/config/SwaggerConfig.java
@@ -4,12 +4,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${api.authUrl}")
+    private String authBaseUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -18,6 +25,9 @@ public class SwaggerConfig {
                         .title("SERVICE AUTH API")
                         .version("1.0")
                         .description("API documentation for the SERVICE AUTH system."))
+                .servers(List.of(new Server()
+                        .url(authBaseUrl)
+                        .description("Auth Service")))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes("Bearer Authentication",
